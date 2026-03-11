1f89c90fc7c7fa5567f8c739f2fa4eabd74baed7
WymsHub <youswma@gmail.com>
2026-03-12 01:37:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/', async (req, res) => {
 });
 
 // IMPORTANT: Railway uses the PORT environment variable
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server Online on Port ${PORT}`);
 });
